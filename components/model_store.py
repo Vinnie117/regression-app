f6a09db367e78c13bebea4bf80be38cd552ce6fe
Vinnie117 <hhang@hotmail.de>
2023-02-07 23:00:42 +0100

    multiple warnings for validate()

--- components/model_store.py
+++ components/model_store.py
@@ -26,10 +26,13 @@ def validate(data, n_clicks):
 
     data = pd.DataFrame(data)
 
+    warning = "Warnung! \n"
+    warn_1 = None
+    warn_2 = None
+
     # check NaNs: true if any NaN in df -> will send confirm dialog
     if data.isnull().values.any():  
-        message = 'Warnung: Daten enthalten leere Werte, die beim Fortfahren verworfen werden'
-        return True, message, None
+        warn_1 = '- Datenfelder enthalten leere Werte, die beim Fortfahren verworfen werden \n'
     
     # type checking for each column 
     # (insight: though column might be of type 'object', the cells can be of mixed primitive types)
@@ -46,8 +49,14 @@ def validate(data, n_clicks):
                 return False, '', None
             except:
                 print("Column: ", i, "mixed data type: ", types) 
-                message = 'Warnung: Gemischte Datentypen'
-                return True, message, None
+                warn_2 = '- Gemischte Datentypen in den Feldern gefunden'
+
+    if warn_1 is not None and warn_2 is not None:
+        return True, warning + warn_1 + warn_2, None
+    elif warn_1 is not None:
+        return True, warning + warn_1, None
+    elif warn_2 is not None:
+        return True, warning + warn_2, None
 
     return False, '', None
     
