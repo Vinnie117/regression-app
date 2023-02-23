317d8625a29665f77e9e85a76cc7aabf63425016
Vinnie117 <hhang@hotmail.de>
2023-02-23 22:27:45 +0100

    UI complete for decimal separator

--- components/table.py
+++ components/table.py
@@ -1,6 +1,10 @@
 from dash.dash_table.Format import Format
 from dash import html, dash_table, dcc
 from assets.data_table import data_table
+from dash_app import dash_app
+from dash.dependencies import Input, Output, State
+from utils.data_prep import numeric_converter
+import pandas as pd
 
 table = html.Div([
 
@@ -118,3 +122,57 @@ table = html.Div([
             }
             )
 ])
+
+@dash_app.callback(
+    Output('table', 'columns'),
+    
+    Output('table', 'data'),
+    # Output('table_store', 'data'),
+
+    Input('decimal_separator', 'value'),
+    Input('table', 'columns'),
+
+    Input('table', 'data'),
+    # Input('table', 'data'),
+
+    prevent_initial_call=True
+)
+def decimal_separator(value, columns,data):
+
+    df = pd.DataFrame(data)
+    # print(df)
+    # print(df.dtypes)
+    df = df.applymap(numeric_converter)
+    json_data = df.to_dict(orient='records')
+
+    if 'Punkt als Dezimaltrennzeichen' in value:
+        decimal = '.'
+    else:
+        decimal = ','
+
+
+    return [
+        {
+            **col,
+            'format': {
+                **col.get('format', {}),
+                'locale': {'decimal': decimal}
+            },
+            'on_change':{
+                'action': 'coerce',
+                'failure': 'accept'
+            }
+        }
+        for col in columns
+
+    ], json_data
+
+
+
+# @dash_app.callback(
+#     Output('table', 'data'),
+#     Input('table_store', 'data')
+# )
+# def clean_data(clean_df):
+#     return clean_df
+
