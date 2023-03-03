23fb6b049a392d3fe09219c4e4482f89b584871b
Vinnie117 <hhang@hotmail.de>
2023-03-03 23:46:06 +0100

    refactor

--- components/validation.py
+++ components/validation.py
@@ -13,8 +13,7 @@ validation =dcc.ConfirmDialog(
     Output('warning_msg', 'displayed'),
     Output('warning_msg', 'message'),
     Output('warning_msg', 'cancel_n_clicks'),
-    # State(component_id = 'table', component_property = 'data'),
-    State(component_id = 'table_store', component_property = 'data'),
+    State(component_id = 'table', component_property = 'data'),
     State(component_id = 'target', component_property = 'value'),
     State(component_id = 'predictors', component_property = 'value'),
     State(component_id = 'controls', component_property = 'value'),               
@@ -26,20 +25,20 @@ def validate(data, target_var, predictor_var, control_vars, n_clicks):
     control_vars = [item for item in control_vars if len(item)>0]
     x_vars = [predictor_var] + control_vars
 
-    data = pd.DataFrame(data)
+    df = pd.DataFrame(data)
 
     warning = "Warnung! \n"
     warn_1 = None
     warn_2 = None
 
     # check NaNs: true if any NaN in df -> will send confirm dialog
-    if data[[target_var] + x_vars].isnull().values.any():  
+    if df[[target_var] + x_vars].isnull().values.any():  
         warn_1 = '- Datenfelder für die Berechnung enthalten fehlerhafte / leere Werte, die beim Fortfahren ignoriert werden \n'
     
     # type checking for each column 
     # (insight: though column might be of type 'object', the cells can be of mixed primitive types)
     for col in [target_var] + x_vars:
-        types = data[col].apply(type).value_counts()
+        types = df[col].apply(type).value_counts()
 
         if len(types) == 1 or (len(types) == 2 and "<class 'NoneType'>" in str(types)):
             print("Column: ", col, "unique data type: ", types)
@@ -47,8 +46,8 @@ def validate(data, target_var, predictor_var, control_vars, n_clicks):
         else:
             try:
                 # user input could be numeric but is interpreted as string -> try to convert
-                data[col] = data[col].astype(float)  # applymap(numeric_converter)
-                # data[col] = data[col].apply(numeric_converter)
+                df[col] = df[col].astype(float)  # applymap(numeric_converter)
+                # df[col] = df[col].apply(numeric_converter)
                 return False, '', None
             except:
                 print("Column: ", col, "mixed data type: ", types) 
