fcb4d3cd5b066530a85a1cc3fb3869870bf3a3e5
Vinnie117 <hhang@hotmail.de>
2023-03-27 15:59:32 +0200

    table css

--- components/table.py
+++ components/table.py
@@ -9,111 +9,106 @@ import json
 
 
 table = html.Div(
-            dash_table.DataTable(
-                id='table',
-                columns=[
-                    {
-                        'name': 'x', 
-                        'id': 'x', 
-                        'type': 'numeric',
-                        'format': {
-                            'locale': {'decimal': ','}, 
-                            'nully': '', 
-                            'prefix': None, 
-                            'specifier': ''
+            className="table",
+            children=
+                dash_table.DataTable(
+                    id='table',
+                    columns=[
+                        {
+                            'name': 'x', 
+                            'id': 'x', 
+                            'type': 'numeric',
+                            'format': {
+                                'locale': {'decimal': ','}, 
+                                'nully': '', 
+                                'prefix': None, 
+                                'specifier': ''
+                            },
+                            'on_change':{
+                                'action': 'coerce',
+                                'failure': 'accept'
+                            },
+                            'deletable': True,
+                            'renamable': True
+                        }, 
+                        {
+                            'name': 'y', 
+                            'id': 'y', 
+                            'type': 'numeric',
+                            'format': {
+                                'locale': {'decimal': ','}, 
+                                'nully': '', 
+                                'prefix': None, 
+                                'specifier': ''
+                            },
+                            'on_change':{
+                                'action': 'coerce',
+                                'failure': 'accept'
+                            },
+                            'deletable': True,
+                            'renamable': True
                         },
-                        'on_change':{
-                            'action': 'coerce',
-                            'failure': 'accept'
+                        {
+                            'name': 'z', 
+                            'id': 'z', 
+                            'type': 'numeric',
+                            'format': {
+                                'locale': {'decimal': ','}, 
+                                'nully': '', 
+                                'prefix': None, 
+                                'specifier': ''
+                            },
+                            'on_change':{
+                                'action': 'coerce',
+                                'failure': 'accept'
+                            },
+                            'deletable': True,
+                            'renamable': True
                         },
-                        'deletable': True,
-                        'renamable': True
-                    }, 
-                    {
-                        'name': 'y', 
-                        'id': 'y', 
-                        'type': 'numeric',
-                        'format': {
-                            'locale': {'decimal': ','}, 
-                            'nully': '', 
-                            'prefix': None, 
-                            'specifier': ''
+                        {
+                            'name': 'a', 
+                            'id': 'a', 
+                            'type': 'numeric',
+                            'format': {
+                                'locale': {'decimal': ','}, 
+                                'nully': '', 
+                                'prefix': None, 
+                                'specifier': ''
+                            },
+                            'on_change':{
+                                'action': 'coerce',
+                                'failure': 'accept'
+                            },
+                            'deletable': True,
+                            'renamable': True
                         },
-                        'on_change':{
-                            'action': 'coerce',
-                            'failure': 'accept'
-                        },
-                        'deletable': True,
-                        'renamable': True
-                    },
-                    {
-                        'name': 'z', 
-                        'id': 'z', 
-                        'type': 'numeric',
-                        'format': {
-                            'locale': {'decimal': ','}, 
-                            'nully': '', 
-                            'prefix': None, 
-                            'specifier': ''
-                        },
-                        'on_change':{
-                            'action': 'coerce',
-                            'failure': 'accept'
-                        },
-                        'deletable': True,
-                        'renamable': True
-                    },
-                    {
-                        'name': 'a', 
-                        'id': 'a', 
-                        'type': 'numeric',
-                        'format': {
-                            'locale': {'decimal': ','}, 
-                            'nully': '', 
-                            'prefix': None, 
-                            'specifier': ''
-                        },
-                        'on_change':{
-                            'action': 'coerce',
-                            'failure': 'accept'
-                        },
-                        'deletable': True,
-                        'renamable': True
-                    },
-                    {
-                        'name': 'b', 
-                        'id': 'b', 
-                        'type': 'numeric',
-                        'format': {
-                            'locale': {'decimal': ','}, 
-                            'nully': '', 
-                            'prefix': None, 
-                            'specifier': ''
-                        },
-                        'on_change':{
-                            'action': 'coerce',
-                            'failure': 'accept'
-                        },
-                        'deletable': True,
-                        'renamable': True
-                    }
+                        {
+                            'name': 'b', 
+                            'id': 'b', 
+                            'type': 'numeric',
+                            'format': {
+                                'locale': {'decimal': ','}, 
+                                'nully': '', 
+                                'prefix': None, 
+                                'specifier': ''
+                            },
+                            'on_change':{
+                                'action': 'coerce',
+                                'failure': 'accept'
+                            },
+                            'deletable': True,
+                            'renamable': True
+                        }
 
-                ],
-                data=data_table,
-                editable=True,
-                fill_width=False,
-                virtualization=True,  # make datatable scrollable
-                style_cell={'width': '100px'}, # width of columns
-                fixed_rows={'headers': True},  #  fix headers for scrolling
-                style_table={'height': '200px', 'overflowY': 'auto'}
-            ), style={
-                'text-align': 'center', 
-                'width': '77%', 
-                'margin-left': 'auto', 
-                'margin-right': 'auto',
-                'border': '1px solid black',
-                # 'display': 'inline-block'
-            }
+                    ],
+                    data=data_table,
+                    editable=True,
+                    fill_width=False,
+                    virtualization=True,  # make datatable scrollable
+                    style_cell={'width': '100px'}, # width of columns
+                    fixed_rows={'headers': True},  #  fix headers for scrolling
+                    style_table={'height': '200px', 'overflowY': 'auto'}
+                )
         )
 
 
