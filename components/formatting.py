cba7edc4a240085cd876216d92458a9ec5c8a5de
Vinnie117 <hhang@hotmail.de>
2023-03-27 14:47:57 +0200

    css for formatting decimal separator

--- components/formatting.py
+++ components/formatting.py
@@ -1,13 +1,12 @@
 from dash import dcc, html
 
 formatting = html.Div(
-                dcc.Checklist(
-                id = 'decimal_separator', 
-                options = ['Punkt als Dezimaltrennzeichen'], 
-                value = [''], inline=True),
-                style={
-                    'width': '77%',
-                    'margin-left': 'auto', 
-                    'margin-right': 'auto',
-                }
-            )
+    className="formatting",
+    children=[
+        dcc.Checklist(
+            id = 'decimal_separator', 
+            options = ['Punkt als Dezimaltrennzeichen'], 
+            value = [''], 
+            inline=True
+    )]
+)
